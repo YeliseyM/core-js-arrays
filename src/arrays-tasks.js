716923bc6bfa43db4bc6c84b56d4be8864bd7a2d
Yelisey Merekin <merekinyelisey@gmail.com>
2024-12-25 10:42:21 +0300

    feat: solve isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -144,8 +144,10 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return true;
+  const firstItemLength = arr[0].length;
+  return arr.every((str) => firstItemLength === str.length);
 }
 
 /**
